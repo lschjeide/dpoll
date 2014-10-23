891e7a745b22efff5458d79ad9d4af531b8ff19a
Leif <lschjeide@gmail.com>
2014-10-23 14:24:58 +1100

    cap deploy jenkins testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.2.1'
 
 set :application, 'dpoll'
 
-#set :repo_url, 'https://github.com/lschjeide/dpoll.git'
+set :repo_url, 'https://github.com/lschjeide/dpoll.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
